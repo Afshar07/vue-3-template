3ad8bd286af60c932056453ffdaf742bfad9081d
@samanfattahi <samanft752@gmail.com>
2023-04-30 10:53:46 +0430

    sidebar fixed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen w-full grid grid-cols-12 bg-gray-100 dark:bg-dark">
     <Overlay></Overlay>
     <Toast></Toast>
-    <div v-if="!fullLayout" class="md:col-span-2 col-span-12">
+    <div v-if="!fullLayout" :class="{'hidden':!sideBarStore.showSideBar}" class="md:col-span-2 md:relative fixed top-0 left-0 z-10 col-span-12 ">
       <side-bar></side-bar>
     </div>
     <div :class="[fullLayout ? '' : 'p-5']" class="md:col-span-10 col-span-12 z-[1]">
@@ -24,7 +24,9 @@ import Toast from "@/components/utilities/Toast.vue";
 import Overlay from "@/components/utilities/Overlay.vue";
 import SideBar from "@/components/main/sideBar.vue";
 import {useAppStore} from "@/stores/app";
+import {useSideBarStore} from "@/stores/sideBar";
 const appStore = useAppStore()
+const sideBarStore = useSideBarStore()
 const route: any = useRoute();
 let fullLayout = computed(() => {
   return route.name !== undefined
