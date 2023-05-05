097d700c4a80ebaf725985da12448d8d75c0fb30
Afshar07 <mr.afshar0@gmail.com>
2023-05-05 17:13:59 +0330

    Change title of page

--- src/router/index.ts
+++ src/router/index.ts
@@ -102,7 +102,7 @@ const router = createRouter({
             component: () => import('../views/chat/index.vue'),
             meta: {
                 auth: true,
-                title: 'داشبورد | گزارشات'
+                title: 'داشبورد | چت'
             }
         },
         {
