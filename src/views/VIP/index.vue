e8aa61d243ab2877e230081e5952e7480b45cefc
saman <samanft752@gmail.com>
2023-05-05 18:34:26 +0430

    VIP Page Demo

--- src/views/VIP/index.vue
+++ src/views/VIP/index.vue
@@ -1,5 +1,29 @@
 <template>
-
+  <div class="md:main-card  grid gap-2 grid-cols-12">
+    <div class="col-span-12 p-3 bg-white flex flex-col items-start rounded-xl shadow">
+      <strong>خرید اشتراک vip</strong>
+      <small class="my-3 border-b pb-3">با خرید اشتراک VIP از مزایای زیر بهره مند شوید</small>
+      <p class="flex-wrap dark:text-white text-gray-500">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+        استفادهلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده ازلورم ایپسوم متن ساختگی با تولید
+        سادگی نامفهوم از صنعت چاپ، و با استفاده از از
+      </p>
+    </div>
+    <div class="col-span-6  bg-white flex flex-col items-center p-3  rounded-xl shadow ">
+      <strong>سالانه</strong>
+      <strong class="my-3">100,000 تومان</strong>
+      <button class=" btn bg-green-500 border-none">پرداخت</button>
+    </div>
+    <div class="col-span-6  bg-white flex flex-col items-center p-3  rounded-xl shadow ">
+      <strong>6 ماهه</strong>
+      <strong class="my-3">50,000 تومان</strong>
+      <button class=" btn bg-green-500 border-none">پرداخت</button>
+    </div>
+    <div class="col-span-6  bg-white flex flex-col items-center p-3  rounded-xl shadow ">
+      <strong>ماهانه</strong>
+      <strong class="my-3">30,000 تومان</strong>
+      <button class=" btn bg-green-500 border-none">پرداخت</button>
+    </div>
+  </div>
 </template>
 
 <script>
