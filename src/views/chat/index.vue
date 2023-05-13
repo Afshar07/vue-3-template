f44065794258b9582942d6d2df86ccd68cc6ce49
Afshar07 <mr.afshar0@gmail.com>
2023-05-13 08:45:24 +0330

    Fix: Fix debounceSearch

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -4,9 +4,13 @@
       <h1>گفتگو های من</h1>
     </header>
     <main>
-      <div class="col-span-12 md:col-span-6  mt-3">
-        <VInput class="col-span-12" v-model="debouncedSearch" :dataType="'text'"
-                :placeHolder="'جستجو...'"></VInput>
+      <div class="col-span-12 md:col-span-6 mt-3">
+        <VInput
+          class="col-span-12"
+          v-model="debouncedSearch"
+          :dataType="'text'"
+          :placeHolder="'جستجو...'"
+        ></VInput>
       </div>
       <div class="stats stats-vertical !w-full shadow-md mt-3">
         <router-link to="/chat/username">
@@ -28,46 +32,45 @@
 
 <script setup lang="ts">
 import ConversationCard from "@/components/utilities/chat/ConversationCard.vue";
-import {computed, inject, onMounted, ref, watch} from "vue";
-import {useAppStore} from "@/stores/app";
+import { computed, inject, onMounted, ref, watch } from "vue";
+import { useAppStore } from "@/stores/app";
 import VInput from "@/components/utilities/VInput.vue";
-const repositories:any = inject('repositories')
-const appStore = useAppStore()
- onMounted(async ()=>{
-  await Promise.all([
-      getMenu()
-  ])
-})
-async function getMenu(){
+const repositories: any = inject("repositories");
+const appStore = useAppStore();
+onMounted(async () => {
+  await Promise.all([getMenu()]);
+});
+async function getMenu() {
   try {
-    appStore.showOverlay = true
-    const res = await  repositories.getMenu.setParams({
-      searchCommand:search.value
-    })
-  }catch (e) {
-    console.log(e)
-  }finally {
-    appStore.showOverlay = false
-
+    appStore.showOverlay = true;
+    const res = await repositories.getMenu.setParams({
+      searchCommand: search.value,
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    appStore.showOverlay = false;
   }
 }
-let search = ref<string>('')
-let timeout = ref<any>(null)
-watch(search,async (val)=>{
-  getMenu()
-})
+let search = ref<string>("");
+let timeout = ref<any>(null);
+watch(search, async (val) => {
+  getMenu();
+});
 let debouncedSearch = computed({
-  get(val:string) {
+  get() {
+    return search.value;
   },
   // setter
-  set(val:string) {
-    timeout = setTimeout(() => {
-     search.value = val
-    }, 2000)
-  }
-
-})
-
+  set(val: string) {
+    if (timeout.value) {
+      clearTimeout(timeout.value);
+    }
+    timeout.value = setTimeout(() => {
+      search.value = val;
+    }, 600);
+  },
+});
 </script>
 
 <style scoped></style>
