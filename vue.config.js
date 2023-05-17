93c6136c611309dfe1391ecaaf276432e3e27044
ASoleymani76 <farhamsj@gmail.com>
2023-05-17 09:48:09 +0330

    #29 - PWA Fixed

--- vue.config.js
+++ vue.config.js
@@ -5,31 +5,5 @@ module.exports = {
         port:5173,
         https:true,
         hotOnly:false
-
     },
-    pwa: {
-        name: 'MaliehIran',
-        themeColor: '#06b6d4',
-        msTileColor: '#7367f0',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        manifestPath:'/manifest.json',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            // swSrc: 'dev/sw.js',
-            // ...other Workbox options...
-        },
-        // manifestOptions:{
-        //     name:'1Minex',
-        //     short_name:'1Minex',
-        //     start_url:'/',
-        //     display:'standalone',
-        //     theme_color:'#4DBA87'
-        // }
-    }
-
-
 };
