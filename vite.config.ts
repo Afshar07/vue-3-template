d78826dd1801d4f8b2f072bc928413c7757d1f93
@samanfattahi <samanft752@gmail.com>
2023-05-20 08:51:00 +0330

    fixed path alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
